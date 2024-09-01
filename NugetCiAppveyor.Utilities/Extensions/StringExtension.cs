cd46807b6ec35e01e02dcdc8b9c4e07af371b08c
Yılmaz Ölmez <yilmazlmez@gmail.com>
2024-09-01 19:21:21 +0300

    Nuget ayarları yapıldı. Test commitidir

---
+++ NugetCiAppveyor.Utilities/Extensions/StringExtension.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace NugetCiAppveyor.Utilities.Extensions
+{
+    public static class StringExtension
+    {
+        public static string ToString(this List<string> value)
+        {
+            return JsonConvert.SerializeObject(value);
+        }
+    }
+}
---
+++ NugetCiAppveyor.Utility/Extensions/StringExtension.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NugetCiAppveyor.Utility.Extensions
+{
+    public static class StringExtension
+    {
+        public static string ToCamelCase(this string str) 
+        { 
+            return str.ToCamelCase();
+        }
+
+        public static string ToSerialize(this List<string> list)
+        {
+            return JsonConvert.SerializeObject(list);
+        }
+    }
+}
